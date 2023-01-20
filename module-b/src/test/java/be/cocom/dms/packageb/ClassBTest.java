377a63f1760a47fd80ca56157570bdf41ebd7564
skafif <skafif@iriscare.brussels>
2023-01-20 20:42:52 +0100

    test fixture test

--- module-b/src/test/java/be/cocom/dms/packageb/ClassBTest.java
+++ module-b/src/test/java/be/cocom/dms/packageb/ClassBTest.java
@@ -1,6 +1,7 @@
 package be.cocom.dms.packageb;
 
 import be.cocom.dms.packagea.ClassA;
+import be.cocom.dms.packagea.TestConstantA;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ class ClassBTest {
 
     @Test
     void testAccessToClassATestClass() {
-        myClassBVariable = TestConstantB.oneStaticSource;
+        myClassBVariable = TestConstantA.oneStaticSource;
         Assertions.assertThat(myClassBVariable).isEqualTo("I'm an static source from A");
     }
 
